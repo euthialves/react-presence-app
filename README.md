8ac3ae780ed56d23d2e5c3109fb9202d46028cf2
Thiago Alves <euthigoalves2023@gmail.com>
2023-06-21 22:54:35 -0300

    Refact code Javascript to Typescript

---
+++ README.md
@@ -0,0 +1,5 @@
+# react-presence-app
+## App Lista de Presença
+
+- Permite um professor fazer a chamada dos alunos 
+- Projeto feito em sala de aula
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react"; //hooks - 
+
+import { Card, ICardProps } from "../../components/Card";
+
+import "./styles.css"
+
+interface IProfileResponse {
+  name: string;
+  avatar_url: string;
+}
+
+interface IUser {
+  name: string;
+  avatar: string;
+}
+
+export function Home() {
+  const [user, setUser] = useState<IUser>({} as IUser)
+  const [students, setStudents] = useState<ICardProps[]>([]);
+  const [studentName, setStudentName] = useState<string>("")
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://api.github.com/users/euthialves")
+      const data = await response.json() as IProfileResponse
+
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url
+      })
+    }
+
+    fetchData()
+  }, [])
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("Pt-Br", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+      }),
+    }
+
+    setStudents((prevState) => [...prevState, newStudent]) // (...) stringOperator - 
+  }
+
+  return (
+    <div className="container">
+      <header>
+        <h1>Lista de Presença</h1>
+
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Avatar" />
+        </div>
+      </header>
+
+      <input
+        type="text"
+        placeholder="Digite o nome aqui..."
+        onChange={(e) => { setStudentName(e.target.value) }}
+      />
+      <button 
+      disabled = {!studentName}
+      onClick={handleAddStudent}>Adicionar</button>
+
+      {students.map((student) => ( // .map é usado para fazer um loop no array e é utilizado para retornar alguma coisa (toda vez que for usado, precisamos passar a propriedade "key" prop)
+        <Card key={student.time} name={student.name} time={student.time} /> // precisamos garantir que essa "key" será única!
+      ))}
+    </div>
+  )
+}
